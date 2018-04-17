d939dfa67d7cfa78728b4eaec9f4a7d925c8d24f
Safa <boufaressafa20@gmail.com>
2018-04-17 13:01:30 +0100

    17-04-2018
    17-04-2018 safa

---
+++ src/Souk/BackBundle/Controller/signalController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Souk\BackBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class signalController extends Controller
+{
+}
