54cceb6b9d4ca280a396cd56b1c6b4fae72a41a2
soumayaZ <soumayaZ@users.noreply.github.com>
2018-04-30 16:17:18 +0100

    Souma:Rec

---
+++ var/cache/dev/twig/cc/cc3ee13d3d52e3373194f089ced62a4f2175f82ac435d9ee573c73fa087182c2.php
@@ -0,0 +1,232 @@
+<?php
+
+/* @Front/Message/threads_list.html.twig */
+class __TwigTemplate_42ec8d2bdbbdd990c3bc1f4d51c093b5204673e0da9aa84b85ed886bf19d3c39 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "@Front/Message/threads_list.html.twig"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "@Front/Message/threads_list.html.twig"));
+
+        // line 1
+        echo "<table class=\"table table-inbox table-hover\">
+    <tbody>
+    ";
+        // line 3
+        if ((isset($context["threads"]) ? $context["threads"] : $this->getContext($context, "threads"))) {
+            // line 4
+            echo "        
+            ";
+            // line 5
+            $context['_parent'] = $context;
+            $context['_seq'] = twig_ensure_traversable((isset($context["threads"]) ? $context["threads"] : $this->getContext($context, "threads")));
+            foreach ($context['_seq'] as $context["_key"] => $context["thread"]) {
+                // line 6
+                echo "                    ";
+                if ( !$this->env->getExtension('FOS\MessageBundle\Twig\Extension\MessageExtension')->isRead($context["thread"])) {
+                    // line 7
+                    echo "                        <tr class=\"unread\">
+                    ";
+                } else {
+                    // line 9
+                    echo "                        <tr class=\"\">
+                    ";
+                }
+                // line 11
+                echo "              
+                  <td class=\"inbox-small-cells\">
+                      ";
+                // line 13
+                if ($this->env->getExtension('FOS\MessageBundle\Twig\Extension\MessageExtension')->canDeleteThread($context["thread"])) {
+                    // line 14
+                    echo "                        ";
+                    if ($this->env->getExtension('FOS\MessageBundle\Twig\Extension\MessageExtension')->isThreadDeletedByParticipant($context["thread"])) {
+                        // line 15
+                        echo "                            ";
+                        ob_start();
+                        echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\RoutingExtension')->getUrl("fos_message_thread_undelete", array("threadId" => $this->getAttribute($context["thread"], "id", array()))), "html", null, true);
+                        $context["formAction"] = ('' === $tmp = ob_get_clean()) ? '' : new Twig_Markup($tmp, $this->env->getCharset());
+                        // line 16
+                        echo "                            ";
+                        ob_start();
+                        echo $this->env->getExtension('Symfony\Bridge\Twig\Extension\TranslationExtension')->getTranslator()->trans("undelete", array(), "FOSMessageBundle");
+                        $context["submitValue"] = ('' === $tmp = ob_get_clean()) ? '' : new Twig_Markup($tmp, $this->env->getCharset());
+                        // line 17
+                        echo "                        ";
+                    } else {
+                        // line 18
+                        echo "                            ";
+                        ob_start();
+                        echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\RoutingExtension')->getUrl("fos_message_thread_delete", array("threadId" => $this->getAttribute($context["thread"], "id", array()))), "html", null, true);
+                        $context["formAction"] = ('' === $tmp = ob_get_clean()) ? '' : new Twig_Markup($tmp, $this->env->getCharset());
+                        // line 19
+                        echo "                            ";
+                        ob_start();
+                        echo $this->env->getExtension('Symfony\Bridge\Twig\Extension\TranslationExtension')->getTranslator()->trans("delete", array(), "FOSMessageBundle");
+                        $context["submitValue"] = ('' === $tmp = ob_get_clean()) ? '' : new Twig_Markup($tmp, $this->env->getCharset());
+                        // line 20
+                        echo "                        ";
+                    }
+                    // line 21
+                    echo "                        <form action=\"";
+                    echo twig_escape_filter($this->env, (isset($context["formAction"]) ? $context["formAction"] : $this->getContext($context, "formAction")), "html", null, true);
+                    echo "\" method=\"post\">
+                                <button class=\"btn-simple text-danger\" type=\"submit\" value=\"";
+                    // line 22
+                    echo twig_escape_filter($this->env, (isset($context["submitValue"]) ? $context["submitValue"] : $this->getContext($context, "submitValue")), "html", null, true);
+                    echo "\" ><i class=\"fa fa-trash\"></i></button>
+                        </form>
+                      <a class=\" text-success\" href=\"";
+                    // line 24
+                    echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\RoutingExtension')->getPath("fos_message_thread_view", array("threadId" => $this->getAttribute($context["thread"], "id", array()))), "html", null, true);
+                    echo "\">  <i class=\"fa fa-eye\"></i></a>
+                    ";
+                }
+                // line 26
+                echo "                  </td>
+                  <td class=\"view-message  dont-show\">";
+                // line 27
+                echo twig_escape_filter($this->env, $this->getAttribute($this->getAttribute($context["thread"], "lastMessage", array()), "sender", array()));
+                echo "</td>
+                  <td class=\"view-message  dont-show\">";
+                // line 28
+                echo twig_escape_filter($this->env, $this->getAttribute($context["thread"], "subject", array()), "html", null, true);
+                echo "</td>
+                  <td class=\"view-message \">
+                      <a href=\"";
+                // line 30
+                echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\RoutingExtension')->getUrl("fos_message_thread_view", array("threadId" => $this->getAttribute($context["thread"], "id", array()))), "html", null, true);
+                echo "#message_";
+                echo twig_escape_filter($this->env, $this->getAttribute($this->getAttribute($context["thread"], "lastMessage", array()), "id", array()), "html", null, true);
+                echo "\" title=\"";
+                echo $this->env->getExtension('Symfony\Bridge\Twig\Extension\TranslationExtension')->getTranslator()->trans("goto_last", array(), "FOSMessageBundle");
+                echo "\">
+                            →
+                        </a></td>
+                  <td class=\"view-message  text-right\">";
+                // line 33
+                echo twig_escape_filter($this->env, twig_date_format_filter($this->env, $this->getAttribute($context["thread"], "createdAt", array()), "d-m-Y H:m"), "html", null, true);
+                echo "</td>
+              </tr>
+              
+            ";
+            }
+            $_parent = $context['_parent'];
+            unset($context['_seq'], $context['_iterated'], $context['_key'], $context['thread'], $context['_parent'], $context['loop']);
+            $context = array_intersect_key($context, $_parent) + $_parent;
+            // line 37
+            echo "    ";
+        } else {
+            // line 38
+            echo "        <tr>
+            <td colspan=\"5\">
+                ";
+            // line 40
+            echo $this->env->getExtension('Symfony\Bridge\Twig\Extension\TranslationExtension')->getTranslator()->trans("no_thread", array(), "FOSMessageBundle");
+            echo ".
+            </td>
+        </tr>
+    ";
+        }
+        // line 44
+        echo "
+    </tbody>
+
+</table>
+";
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "@Front/Message/threads_list.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  145 => 44,  138 => 40,  134 => 38,  131 => 37,  121 => 33,  111 => 30,  106 => 28,  102 => 27,  99 => 26,  94 => 24,  89 => 22,  84 => 21,  81 => 20,  76 => 19,  71 => 18,  68 => 17,  63 => 16,  58 => 15,  55 => 14,  53 => 13,  49 => 11,  45 => 9,  41 => 7,  38 => 6,  34 => 5,  31 => 4,  29 => 3,  25 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("<table class=\"table table-inbox table-hover\">
+    <tbody>
+    {% if threads %}
+        
+            {% for thread in threads %}
+                    {% if not fos_message_is_read(thread) %}
+                        <tr class=\"unread\">
+                    {% else %}
+                        <tr class=\"\">
+                    {% endif %}
+              
+                  <td class=\"inbox-small-cells\">
+                      {% if fos_message_can_delete_thread(thread) %}
+                        {% if fos_message_deleted_by_participant(thread) %}
+                            {% set formAction %}{{ url('fos_message_thread_undelete', {'threadId': thread.id}) }}{% endset %}
+                            {% set submitValue %}{% trans from 'FOSMessageBundle' %}undelete{% endtrans %}{% endset %}
+                        {% else %}
+                            {% set formAction %}{{ url('fos_message_thread_delete', {'threadId': thread.id}) }}{% endset %}
+                            {% set submitValue %}{% trans from 'FOSMessageBundle' %}delete{% endtrans %}{% endset %}
+                        {% endif %}
+                        <form action=\"{{ formAction }}\" method=\"post\">
+                                <button class=\"btn-simple text-danger\" type=\"submit\" value=\"{{ submitValue }}\" ><i class=\"fa fa-trash\"></i></button>
+                        </form>
+                      <a class=\" text-success\" href=\"{{ path('fos_message_thread_view', {'threadId': thread.id}) }}\">  <i class=\"fa fa-eye\"></i></a>
+                    {% endif %}
+                  </td>
+                  <td class=\"view-message  dont-show\">{{thread.lastMessage.sender|e }}</td>
+                  <td class=\"view-message  dont-show\">{{thread.subject }}</td>
+                  <td class=\"view-message \">
+                      <a href=\"{{ url('fos_message_thread_view', {'threadId': thread.id}) }}#message_{{ thread.lastMessage.id }}\" title=\"{% trans from 'FOSMessageBundle' %}goto_last{% endtrans %}\">
+                            →
+                        </a></td>
+                  <td class=\"view-message  text-right\">{{ thread.createdAt|date('d-m-Y H:m') }}</td>
+              </tr>
+              
+            {% endfor %}
+    {% else %}
+        <tr>
+            <td colspan=\"5\">
+                {% trans from 'FOSMessageBundle' %}no_thread{% endtrans %}.
+            </td>
+        </tr>
+    {% endif %}
+
+    </tbody>
+
+</table>
+", "@Front/Message/threads_list.html.twig", "C:\\Users\\Soumaya\\Documents\\GitHub\\Souk\\src\\Souk\\FrontBundle\\Resources\\views\\Message\\threads_list.html.twig");
+    }
+}
---
+++ var/cache/dev/twig/e5/e52b09fc2643158f04a5738cfb7a0cff82b9753c7a0603aafdef3c4dbffa562d.php
@@ -0,0 +1,192 @@
+<?php
+
+/* BackBundle:reclamations:index.html.twig */
+class __TwigTemplate_3d98cbcc4ef4075a9b32e4c0b7a75408e790d50e252c7da31fb125aa8fd90197 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        // line 1
+        $this->parent = $this->loadTemplate("BackBundle::panel.html.twig", "BackBundle:reclamations:index.html.twig", 1);
+        $this->blocks = array(
+            'body' => array($this, 'block_body'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "BackBundle::panel.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "BackBundle:reclamations:index.html.twig"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "BackBundle:reclamations:index.html.twig"));
+
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+    }
+
+    // line 2
+    public function block_body($context, array $blocks = array())
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+
+        // line 3
+        echo "    <h1> Liste Reclamations</h1>
+    <table class=\"table table-striped\">
+        <thead class=\"thead-light\">
+            <tr class=\"table-secondary\">
+                <th>Contenu</th>
+                <th>Commercial</th>
+                <th>Date de reclamation</th>
+                <th>Etat</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+        ";
+        // line 15
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable((isset($context["reclamations"]) ? $context["reclamations"] : $this->getContext($context, "reclamations")));
+        foreach ($context['_seq'] as $context["_key"] => $context["reclamation"]) {
+            // line 16
+            echo "            <tr>
+
+                <td>";
+            // line 18
+            echo twig_escape_filter($this->env, $this->getAttribute($context["reclamation"], "contenu", array()), "html", null, true);
+            echo "</td>
+                <td> ";
+            // line 19
+            echo twig_escape_filter($this->env, $this->getAttribute($this->getAttribute($context["reclamation"], "commercial", array()), "nom", array()), "html", null, true);
+            echo "</td>
+                <td>";
+            // line 20
+            if ($this->getAttribute($context["reclamation"], "dateRec", array())) {
+                echo twig_escape_filter($this->env, twig_date_format_filter($this->env, $this->getAttribute($context["reclamation"], "dateRec", array()), "Y-m-d"), "html", null, true);
+            }
+            echo "</td>
+                <td>
+                    ";
+            // line 22
+            if (($this->getAttribute($context["reclamation"], "etat", array()) == 0)) {
+                echo " encours
+                    ";
+            } elseif (($this->getAttribute(            // line 23
+$context["reclamation"], "etat", array()) == 1)) {
+                echo " acceptée
+                    ";
+            } elseif (($this->getAttribute(            // line 24
+$context["reclamation"], "etat", array()) ==  -1)) {
+                echo "  rejetée
+                    ";
+            }
+            // line 26
+            echo "                </td>
+                <td>
+                    <a class=\"btn btn-success\" href=\"";
+            // line 28
+            echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\RoutingExtension')->getPath("admin_reclamations_accepter", array("id" => $this->getAttribute($context["reclamation"], "id", array()))), "html", null, true);
+            echo "\"> <i class=\"fa fa-check \" ></i></a>
+                    <a class=\"btn btn-danger\" href=\"";
+            // line 29
+            echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\RoutingExtension')->getPath("admin_reclamations_refuser", array("id" => $this->getAttribute($context["reclamation"], "id", array()))), "html", null, true);
+            echo "\"> <i class=\"fa fa-times\" ></i></a>
+
+                </td>
+            </tr>
+        ";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['reclamation'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 34
+        echo "        </tbody>
+    </table>
+";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "BackBundle:reclamations:index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  118 => 34,  107 => 29,  103 => 28,  99 => 26,  94 => 24,  90 => 23,  86 => 22,  79 => 20,  75 => 19,  71 => 18,  67 => 16,  63 => 15,  49 => 3,  40 => 2,  11 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("{% extends 'BackBundle::panel.html.twig' %}
+{% block body %}
+    <h1> Liste Reclamations</h1>
+    <table class=\"table table-striped\">
+        <thead class=\"thead-light\">
+            <tr class=\"table-secondary\">
+                <th>Contenu</th>
+                <th>Commercial</th>
+                <th>Date de reclamation</th>
+                <th>Etat</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+        {% for reclamation in reclamations %}
+            <tr>
+
+                <td>{{ reclamation.contenu }}</td>
+                <td> {{ reclamation.commercial.nom }}</td>
+                <td>{% if reclamation.dateRec %}{{ reclamation.dateRec|date('Y-m-d') }}{% endif %}</td>
+                <td>
+                    {% if reclamation.etat == 0 %} encours
+                    {% elseif reclamation.etat == 1 %} acceptée
+                    {% elseif reclamation.etat == (-1) %}  rejetée
+                    {% endif %}
+                </td>
+                <td>
+                    <a class=\"btn btn-success\" href=\"{{ path('admin_reclamations_accepter', { 'id': reclamation.id } ) }}\"> <i class=\"fa fa-check \" ></i></a>
+                    <a class=\"btn btn-danger\" href=\"{{ path('admin_reclamations_refuser', { 'id': reclamation.id } ) }}\"> <i class=\"fa fa-times\" ></i></a>
+
+                </td>
+            </tr>
+        {% endfor %}
+        </tbody>
+    </table>
+{% endblock %}
+", "BackBundle:reclamations:index.html.twig", "C:\\Users\\Soumaya\\Documents\\GitHub\\Souk\\src\\Souk\\BackBundle\\Resources\\views\\reclamations\\index.html.twig");
+    }
+}
