f137584c9650294d645526998eb4e06092a82843
soumayaZ <soumayaZ@users.noreply.github.com>
2018-05-08 18:55:47 +0100

    Commit:so

--- src/Souk/ApiBundle/Controller/UserApiController.php
+++ src/Souk/ApiBundle/Controller/UserApiController.php
@@ -7,41 +7,43 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
 
 class UserApiController extends Controller
 {
 
 ///se connecter à traver l'application
-  public function getUserAction($login, $password)
-  {
+    public function getUserAction($login, $password)
+    {
+
+        $normalizer = new ObjectNormalizer();
+        $normalizer->setCircularReferenceLimit(1);
 
 
-    /*$errors = [];
-    $errors[] = 'succ';
-*/
-    $passwordEncoder = $this->get('security.password_encoder');
+        $serializer = new   Serializer([$normalizer]);
+        $normalizer->setCircularReferenceHandler(function ($object) {
+            return $object->getId();
+        });
+        $passwordEncoder = $this->get('security.password_encoder');
 
-    $serializer = new   Serializer([new ObjectNormalizer()]);
-    $user = $this->getDoctrine()->getManager()
-      ->getRepository('UserBundle:User')
-      ->findBy(array("username" => $login));
-    if (!$passwordEncoder->isPasswordValid($user[0], $password, $user[0]->getSalt())) {
-      $formatted = $serializer->normalize('erreur', 'json');
+        $user = $this->getDoctrine()->getManager()
+            ->getRepository('UserBundle:User')
+            ->findBy(array("username" => $login));
+        if (!$passwordEncoder->isPasswordValid($user[0], $password, $user[0]->getSalt())) {
 
-      return new JsonResponse($formatted);
+
+            $formatted = $serializer->normalize('erreur', 'json');
+
+            return new JsonResponse($formatted);
+
+        } else {
+
+            $formatted = $serializer->normalize($user, 'json');
+            return new JsonResponse($formatted);
+
+        }
+
 
     }
-    else{
-
-      $formatted = $serializer->normalize($user, 'json');
-      return new JsonResponse($formatted);
-
-    }
-
-
-
-
-  }
 
 }
