caaf46d86aaeaad57e2f1f02913516c62e0dc28c
salsabiil <salsabil.dridi1@gmail.com>
2018-06-02 03:14:49 +0100

    evenement edit + list participants corr

---
+++ var/cache/dev/twig/06/066748accaf5dcfc9bec7b65ce50df1ee4bc1759535b41d22d94112676938cfc.php
@@ -0,0 +1,250 @@
+<?php
+
+/* FrontBundle:evennements:edit.html.twig */
+class __TwigTemplate_0ec08412f66c1e9903eb5e94fa4f046c170c2de88b87a0e46234b2938871833d extends Twig_Template
+{
+    private $source;
+
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        // line 1
+        $this->parent = $this->loadTemplate("FrontBundle::layout.html.twig", "FrontBundle:evennements:edit.html.twig", 1);
+        $this->blocks = array(
+            'body' => array($this, 'block_body'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "FrontBundle::layout.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "FrontBundle:evennements:edit.html.twig"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "FrontBundle:evennements:edit.html.twig"));
+
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+    }
+
+    // line 3
+    public function block_body($context, array $blocks = array())
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+
+        // line 4
+        echo "    <h1>Evennement edit</h1>
+";
+        // line 5
+        echo         $this->env->getRuntime('Symfony\Bridge\Twig\Form\TwigRenderer')->renderBlock((isset($context["edit_form"]) || array_key_exists("edit_form", $context) ? $context["edit_form"] : (function () { throw new Twig_Error_Runtime('Variable "edit_form" does not exist.', 5, $this->source); })()), 'form_start');
+        echo "
+       ";
+        // line 7
+        echo "<div class=\"col-md-12\">
+    <div class=\"col-md-6\">
+
+        ";
+        // line 10
+        echo $this->env->getRuntime('Symfony\Bridge\Twig\Form\TwigRenderer')->searchAndRenderBlock(twig_get_attribute($this->env, $this->source, (isset($context["edit_form"]) || array_key_exists("edit_form", $context) ? $context["edit_form"] : (function () { throw new Twig_Error_Runtime('Variable "edit_form" does not exist.', 10, $this->source); })()), "titre", array()), 'label');
+        echo "
+        ";
+        // line 11
+        echo $this->env->getRuntime('Symfony\Bridge\Twig\Form\TwigRenderer')->searchAndRenderBlock(twig_get_attribute($this->env, $this->source, (isset($context["edit_form"]) || array_key_exists("edit_form", $context) ? $context["edit_form"] : (function () { throw new Twig_Error_Runtime('Variable "edit_form" does not exist.', 11, $this->source); })()), "titre", array()), 'errors');
+        echo "
+        ";
+        // line 12
+        echo $this->env->getRuntime('Symfony\Bridge\Twig\Form\TwigRenderer')->searchAndRenderBlock(twig_get_attribute($this->env, $this->source, (isset($context["edit_form"]) || array_key_exists("edit_form", $context) ? $context["edit_form"] : (function () { throw new Twig_Error_Runtime('Variable "edit_form" does not exist.', 12, $this->source); })()), "titre", array()), 'widget', array("attr" => array("class" => "comment form-control")));
+        echo "
+    </div>
+    <div class=\"col-md-6\">
+        ";
+        // line 15
+        echo $this->env->getRuntime('Symfony\Bridge\Twig\Form\TwigRenderer')->searchAndRenderBlock(twig_get_attribute($this->env, $this->source, (isset($context["edit_form"]) || array_key_exists("edit_form", $context) ? $context["edit_form"] : (function () { throw new Twig_Error_Runtime('Variable "edit_form" does not exist.', 15, $this->source); })()), "description", array()), 'label');
+        echo "
+        ";
+        // line 16
+        echo $this->env->getRuntime('Symfony\Bridge\Twig\Form\TwigRenderer')->searchAndRenderBlock(twig_get_attribute($this->env, $this->source, (isset($context["edit_form"]) || array_key_exists("edit_form", $context) ? $context["edit_form"] : (function () { throw new Twig_Error_Runtime('Variable "edit_form" does not exist.', 16, $this->source); })()), "description", array()), 'errors');
+        echo "
+        ";
+        // line 17
+        echo $this->env->getRuntime('Symfony\Bridge\Twig\Form\TwigRenderer')->searchAndRenderBlock(twig_get_attribute($this->env, $this->source, (isset($context["edit_form"]) || array_key_exists("edit_form", $context) ? $context["edit_form"] : (function () { throw new Twig_Error_Runtime('Variable "edit_form" does not exist.', 17, $this->source); })()), "description", array()), 'widget', array("attr" => array("class" => "comment form-control")));
+        echo "
+        ";
+        // line 18
+        echo         $this->env->getRuntime('Symfony\Bridge\Twig\Form\TwigRenderer')->renderBlock((isset($context["edit_form"]) || array_key_exists("edit_form", $context) ? $context["edit_form"] : (function () { throw new Twig_Error_Runtime('Variable "edit_form" does not exist.', 18, $this->source); })()), 'form_start');
+        echo "
+    </div>
+</div>
+<div class=\"col-md-12\">
+    <div class=\"col-md-6\">
+        ";
+        // line 23
+        echo $this->env->getRuntime('Symfony\Bridge\Twig\Form\TwigRenderer')->searchAndRenderBlock(twig_get_attribute($this->env, $this->source, (isset($context["edit_form"]) || array_key_exists("edit_form", $context) ? $context["edit_form"] : (function () { throw new Twig_Error_Runtime('Variable "edit_form" does not exist.', 23, $this->source); })()), "dateDeb", array()), 'label');
+        echo "
+        ";
+        // line 24
+        echo $this->env->getRuntime('Symfony\Bridge\Twig\Form\TwigRenderer')->searchAndRenderBlock(twig_get_attribute($this->env, $this->source, (isset($context["edit_form"]) || array_key_exists("edit_form", $context) ? $context["edit_form"] : (function () { throw new Twig_Error_Runtime('Variable "edit_form" does not exist.', 24, $this->source); })()), "dateDeb", array()), 'errors');
+        echo "
+        ";
+        // line 25
+        echo $this->env->getRuntime('Symfony\Bridge\Twig\Form\TwigRenderer')->searchAndRenderBlock(twig_get_attribute($this->env, $this->source, (isset($context["edit_form"]) || array_key_exists("edit_form", $context) ? $context["edit_form"] : (function () { throw new Twig_Error_Runtime('Variable "edit_form" does not exist.', 25, $this->source); })()), "dateDeb", array()), 'widget', array("attr" => array("class" => "form-group input-group")));
+        echo "
+    </div>
+    <div class=\"col-md-6\">
+        ";
+        // line 28
+        echo $this->env->getRuntime('Symfony\Bridge\Twig\Form\TwigRenderer')->searchAndRenderBlock(twig_get_attribute($this->env, $this->source, (isset($context["edit_form"]) || array_key_exists("edit_form", $context) ? $context["edit_form"] : (function () { throw new Twig_Error_Runtime('Variable "edit_form" does not exist.', 28, $this->source); })()), "dateFin", array()), 'label');
+        echo "
+        ";
+        // line 29
+        echo $this->env->getRuntime('Symfony\Bridge\Twig\Form\TwigRenderer')->searchAndRenderBlock(twig_get_attribute($this->env, $this->source, (isset($context["edit_form"]) || array_key_exists("edit_form", $context) ? $context["edit_form"] : (function () { throw new Twig_Error_Runtime('Variable "edit_form" does not exist.', 29, $this->source); })()), "dateFin", array()), 'errors');
+        echo "
+        ";
+        // line 30
+        echo $this->env->getRuntime('Symfony\Bridge\Twig\Form\TwigRenderer')->searchAndRenderBlock(twig_get_attribute($this->env, $this->source, (isset($context["edit_form"]) || array_key_exists("edit_form", $context) ? $context["edit_form"] : (function () { throw new Twig_Error_Runtime('Variable "edit_form" does not exist.', 30, $this->source); })()), "dateFin", array()), 'widget', array("attr" => array("class" => "form-group input-group")));
+        echo "
+    </div>
+</div>
+<div class=\"col-md-12\">
+    <div class=\"col-md-6\">
+        ";
+        // line 35
+        echo $this->env->getRuntime('Symfony\Bridge\Twig\Form\TwigRenderer')->searchAndRenderBlock(twig_get_attribute($this->env, $this->source, (isset($context["edit_form"]) || array_key_exists("edit_form", $context) ? $context["edit_form"] : (function () { throw new Twig_Error_Runtime('Variable "edit_form" does not exist.', 35, $this->source); })()), "lieu", array()), 'label');
+        echo "
+        ";
+        // line 36
+        echo $this->env->getRuntime('Symfony\Bridge\Twig\Form\TwigRenderer')->searchAndRenderBlock(twig_get_attribute($this->env, $this->source, (isset($context["edit_form"]) || array_key_exists("edit_form", $context) ? $context["edit_form"] : (function () { throw new Twig_Error_Runtime('Variable "edit_form" does not exist.', 36, $this->source); })()), "lieu", array()), 'errors');
+        echo "
+        ";
+        // line 37
+        echo $this->env->getRuntime('Symfony\Bridge\Twig\Form\TwigRenderer')->searchAndRenderBlock(twig_get_attribute($this->env, $this->source, (isset($context["edit_form"]) || array_key_exists("edit_form", $context) ? $context["edit_form"] : (function () { throw new Twig_Error_Runtime('Variable "edit_form" does not exist.', 37, $this->source); })()), "lieu", array()), 'widget', array("attr" => array("class" => "comment form-control")));
+        echo "
+    </div>
+    <div class=\"col-md-6\">
+        ";
+        // line 40
+        echo $this->env->getRuntime('Symfony\Bridge\Twig\Form\TwigRenderer')->searchAndRenderBlock(twig_get_attribute($this->env, $this->source, (isset($context["edit_form"]) || array_key_exists("edit_form", $context) ? $context["edit_form"] : (function () { throw new Twig_Error_Runtime('Variable "edit_form" does not exist.', 40, $this->source); })()), "prix", array()), 'label');
+        echo "
+        ";
+        // line 41
+        echo $this->env->getRuntime('Symfony\Bridge\Twig\Form\TwigRenderer')->searchAndRenderBlock(twig_get_attribute($this->env, $this->source, (isset($context["edit_form"]) || array_key_exists("edit_form", $context) ? $context["edit_form"] : (function () { throw new Twig_Error_Runtime('Variable "edit_form" does not exist.', 41, $this->source); })()), "prix", array()), 'errors');
+        echo "
+        ";
+        // line 42
+        echo $this->env->getRuntime('Symfony\Bridge\Twig\Form\TwigRenderer')->searchAndRenderBlock(twig_get_attribute($this->env, $this->source, (isset($context["edit_form"]) || array_key_exists("edit_form", $context) ? $context["edit_form"] : (function () { throw new Twig_Error_Runtime('Variable "edit_form" does not exist.', 42, $this->source); })()), "prix", array()), 'widget', array("attr" => array("class" => "comment form-control")));
+        echo "
+    </div>
+</div>
+
+<div class=\"col-md-12\">
+    <div class=\"col-md-6\">
+            <button type=\"submit\" class=\"btn btn-default\" > <i class=\"fa fa-trash\"></i> Delete </button>
+            <a class=\"btn btn-success\" href=\"";
+        // line 49
+        echo $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("evennements_index");
+        echo "\"><i class=\"fa fa-arrow-left\" > Back to the liste </i> </a>
+</div>
+</div>
+    ";
+        // line 52
+        echo         $this->env->getRuntime('Symfony\Bridge\Twig\Form\TwigRenderer')->renderBlock((isset($context["edit_form"]) || array_key_exists("edit_form", $context) ? $context["edit_form"] : (function () { throw new Twig_Error_Runtime('Variable "edit_form" does not exist.', 52, $this->source); })()), 'form_end');
+        echo "
+";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "FrontBundle:evennements:edit.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  167 => 52,  161 => 49,  151 => 42,  147 => 41,  143 => 40,  137 => 37,  133 => 36,  129 => 35,  121 => 30,  117 => 29,  113 => 28,  107 => 25,  103 => 24,  99 => 23,  91 => 18,  87 => 17,  83 => 16,  79 => 15,  73 => 12,  69 => 11,  65 => 10,  60 => 7,  56 => 5,  53 => 4,  44 => 3,  15 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("{% extends 'FrontBundle::layout.html.twig' %}
+
+{% block body %}
+    <h1>Evennement edit</h1>
+{{ form_start(edit_form) }}
+       {#{{ form_widget(form) }}#}
+<div class=\"col-md-12\">
+    <div class=\"col-md-6\">
+
+        {{ form_label(edit_form.titre) }}
+        {{ form_errors(edit_form.titre) }}
+        {{ form_widget(edit_form.titre, {'attr': {'class': 'comment form-control'}}) }}
+    </div>
+    <div class=\"col-md-6\">
+        {{ form_label(edit_form.description) }}
+        {{ form_errors(edit_form.description) }}
+        {{ form_widget(edit_form.description, {'attr': {'class': 'comment form-control'}}) }}
+        {{ form_start(edit_form) }}
+    </div>
+</div>
+<div class=\"col-md-12\">
+    <div class=\"col-md-6\">
+        {{ form_label(edit_form.dateDeb) }}
+        {{ form_errors(edit_form.dateDeb) }}
+        {{ form_widget(edit_form.dateDeb, {'attr': {'class': 'form-group input-group'}}) }}
+    </div>
+    <div class=\"col-md-6\">
+        {{ form_label(edit_form.dateFin) }}
+        {{ form_errors(edit_form.dateFin) }}
+        {{ form_widget(edit_form.dateFin, {'attr': {'class': 'form-group input-group'}}) }}
+    </div>
+</div>
+<div class=\"col-md-12\">
+    <div class=\"col-md-6\">
+        {{ form_label(edit_form.lieu) }}
+        {{ form_errors(edit_form.lieu) }}
+        {{ form_widget(edit_form.lieu, {'attr': {'class': 'comment form-control'}}) }}
+    </div>
+    <div class=\"col-md-6\">
+        {{ form_label(edit_form.prix) }}
+        {{ form_errors(edit_form.prix) }}
+        {{ form_widget(edit_form.prix, {'attr': {'class': 'comment form-control'}}) }}
+    </div>
+</div>
+
+<div class=\"col-md-12\">
+    <div class=\"col-md-6\">
+            <button type=\"submit\" class=\"btn btn-default\" > <i class=\"fa fa-trash\"></i> Delete </button>
+            <a class=\"btn btn-success\" href=\"{{ path('evennements_index') }}\"><i class=\"fa fa-arrow-left\" > Back to the liste </i> </a>
+</div>
+</div>
+    {{ form_end(edit_form) }}
+{% endblock %}
+", "FrontBundle:evennements:edit.html.twig", "C:\\Users\\salsa\\OneDrive\\Documents\\GitHub\\Souk\\src\\Souk\\FrontBundle/Resources/views/evennements/edit.html.twig");
+    }
+}
---
+++ var/cache/dev/twig/93/9324d220e28c3a68afc27205d708467faf871182a0c1ab1479808faeef14f682.php
@@ -0,0 +1,145 @@
+<?php
+
+/* FrontBundle:evennements:liste_des_participants.html.twig */
+class __TwigTemplate_700018509575b23813b953d8a989ce27280117dde1a8f9814650c860dd76334a extends Twig_Template
+{
+    private $source;
+
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        // line 1
+        $this->parent = $this->loadTemplate("FrontBundle::layout.html.twig", "FrontBundle:evennements:liste_des_participants.html.twig", 1);
+        $this->blocks = array(
+            'body' => array($this, 'block_body'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "FrontBundle::layout.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "FrontBundle:evennements:liste_des_participants.html.twig"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "FrontBundle:evennements:liste_des_participants.html.twig"));
+
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+    }
+
+    // line 3
+    public function block_body($context, array $blocks = array())
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+
+        // line 4
+        echo "    <table class=\"table table-condensed\">
+        <thead>
+        <tr>
+            <th>Date reservation</th>
+            <th>Client</th>
+        </tr>
+        </thead>
+        <tbody>
+        ";
+        // line 12
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable((isset($context["parts"]) || array_key_exists("parts", $context) ? $context["parts"] : (function () { throw new Twig_Error_Runtime('Variable "parts" does not exist.', 12, $this->source); })()));
+        foreach ($context['_seq'] as $context["_key"] => $context["p"]) {
+            // line 13
+            echo "
+            <tr>
+                <td>";
+            // line 15
+            echo twig_escape_filter($this->env, twig_date_format_filter($this->env, twig_get_attribute($this->env, $this->source, $context["p"], "dateRes", array()), "d-m-Y"), "html", null, true);
+            echo "</td>
+                <td>";
+            // line 16
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, $context["p"], "client", array()), "prenom", array()), "html", null, true);
+            echo " ";
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, $context["p"], "client", array()), "nom", array()), "html", null, true);
+            echo " </td>
+            </tr>
+        ";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['p'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 19
+        echo "        </tbody>
+    </table>
+    <li>
+        <a  class=\"btn btn-default\" href=\"";
+        // line 22
+        echo $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("evennements_index");
+        echo "\">Back to Événements <i class=\"fa fa-backward\"> </i> </a>
+    </li>
+";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "FrontBundle:evennements:liste_des_participants.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  91 => 22,  86 => 19,  75 => 16,  71 => 15,  67 => 13,  63 => 12,  53 => 4,  44 => 3,  15 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("{% extends 'FrontBundle::layout.html.twig' %}
+
+{% block body %}
+    <table class=\"table table-condensed\">
+        <thead>
+        <tr>
+            <th>Date reservation</th>
+            <th>Client</th>
+        </tr>
+        </thead>
+        <tbody>
+        {% for p in parts %}
+
+            <tr>
+                <td>{{ p.dateRes|date('d-m-Y') }}</td>
+                <td>{{ p.client.prenom  }} {{ p.client.nom  }} </td>
+            </tr>
+        {% endfor %}
+        </tbody>
+    </table>
+    <li>
+        <a  class=\"btn btn-default\" href=\"{{ path('evennements_index') }}\">Back to Événements <i class=\"fa fa-backward\"> </i> </a>
+    </li>
+{% endblock %}", "FrontBundle:evennements:liste_des_participants.html.twig", "C:\\Users\\salsa\\OneDrive\\Documents\\GitHub\\Souk\\src\\Souk\\FrontBundle/Resources/views/evennements/liste_des_participants.html.twig");
+    }
+}
