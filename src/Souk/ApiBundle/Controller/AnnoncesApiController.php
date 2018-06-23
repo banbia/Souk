680ac64b49c45e9b93f0aa02be8dfbb620cf00bd
haifa-02 <haifa.daoud@esprit.tn>
2018-06-23 17:52:49 +0200

    api annonces
    api annonces

--- src/Souk/ApiBundle/Controller/AnnoncesApiController.php
+++ src/Souk/ApiBundle/Controller/AnnoncesApiController.php
@@ -31,23 +31,28 @@ class AnnoncesApiController extends Controller
     return new JsonResponse($formatted);
   }
   //nouvelle annonce
-  public function createAction(Request $request,$annonce,$date,$quantite,$client)
+  public function createAction($commercial,$prix,$categorie,$description,$titre)
   {
     //connexion
     $em = $this->getDoctrine()->getManager();
-    //get annonce and client object
-    $client = $em->getRepository('UserBundle:User')->find($client);
-    $annonce = $em->getRepository('BackBundle:Annonces')->find($annonce);
-    //instance commande
-    $commande= new Commandes();
-    //affecter les champs
-    $commande->setAnnonce($annonce);
-    $commande->setClient($client);
-    $commande->setQuantite($quantite);
-    $commande->setEtat(0);
-    $commande->setDateCom(new \DateTime($date));
+    //get commercial and categorie object
+    $commercial = $em->getRepository('UserBundle:User')->find($commercial);
+    $categorie = $em->getRepository('BackBundle:Categories')->find($categorie);
 
-    $em->persist($commande);
+    //instance annonce
+    $annonce= new Annonces();
+    //affecter les champs
+    $annonce->setCommercial($commercial);
+    $annonce->setCategorie($categorie);
+    $annonce->setDescription($description);
+    $annonce->setTitre($titre);
+
+    $annonce->setPrix($prix);
+    $annonce->setDisponible(true);
+   
+
+    $annonce->setDateCreation(new \DateTime('now'));
+    $em->persist($annonce);
 
     $em->flush();
     //format date
@@ -61,19 +66,104 @@ class AnnoncesApiController extends Controller
     $normalizer = new ObjectNormalizer();
     //ne pas envoyer client,annonce,commentaires dans le retour json
     $normalizer->setIgnoredAttributes(array('client','annonce','commentaires'));
-    $normalizer->setCallbacks(array('dateCom' => $callback));
+    $normalizer->setCallbacks(array('dateCretaion' => $callback));
     $normalizer->setCircularReferenceLimit(1);
     $serializer = new Serializer([$normalizer]);
     $normalizer->setCircularReferenceHandler(function ($object) {
       return $object->getId();
     });
 
-    $formatted= $serializer->normalize($commande, 'json');
+    $formatted= $serializer->normalize($annonce, 'json');
 
     return new JsonResponse($formatted);
 
   }
 
+//modifier annonce
+  public function modifierAction($idannonces,$prix,$categorie,$description,$titre)
+  {
+    //connexion
+    $em = $this->getDoctrine()->getManager();
+    //get annonces and categorie object
+    $annonce = $em->getRepository('BackBundle:Annonces')->find($idannonces);
+    $categorie = $em->getRepository('BackBundle:Categories')->find($categorie);
+
+
+    //affecter les champs
+
+    $annonce->setCategorie($categorie);
+    $annonce->setDescription($description);
+    $annonce->setTitre($titre);
+
+    $annonce->setPrix($prix);
+
+
+
+    $em->persist($annonce);
+
+    $em->flush();
+    //format date
+    $callback = function ($dateTime) {
+      return $dateTime instanceof \DateTime
+        ? $dateTime->format('Y-m-d')
+        : '';
+    };
+
+
+    $normalizer = new ObjectNormalizer();
+    //ne pas envoyer client,annonce,commentaires dans le retour json
+    $normalizer->setIgnoredAttributes(array('client','annonce','commentaires'));
+    $normalizer->setCallbacks(array('dateCretaion' => $callback));
+    $normalizer->setCircularReferenceLimit(1);
+    $serializer = new Serializer([$normalizer]);
+    $normalizer->setCircularReferenceHandler(function ($object) {
+      return $object->getId();
+    });
+
+    $formatted= $serializer->normalize($annonce, 'json');
+
+    return new JsonResponse($formatted);
+
+  }
+  ///delete annonce
+
+  public function supprimerAction($id)
+  {
+    $em = $this->getDoctrine()->getManager();
+    $annonce = $em->getRepository('BackBundle:Annonces')->find($id);
+    $images = $em->getRepository('BackBundle:Images')->findBy(array("annonce" => $id));
+    foreach ($images as $img ) {
+      $em->remove($img);
+    }
+    $em->remove($annonce);
+    $em->flush();
+
+
+
+    $annonces = $em->getRepository('BackBundle:Annonces')->findAll();
+
+    //format date
+    $callback = function ($dateTime) {
+      return $dateTime instanceof \DateTime
+        ? $dateTime->format('Y-m-d')
+        : '';
+    };
+
+
+    $normalizer = new ObjectNormalizer();
+    //ne pas envoyer client,annonce,commentaires dans le retour json
+    $normalizer->setIgnoredAttributes(array('client','annonce','commentaires'));
+    $normalizer->setCallbacks(array('dateCretaion' => $callback));
+    $normalizer->setCircularReferenceLimit(1);
+    $serializer = new Serializer([$normalizer]);
+    $normalizer->setCircularReferenceHandler(function ($object) {
+      return $object->getId();
+    });
+
+    $formatted= $serializer->normalize($annonces, 'json');
+
+    return new JsonResponse($formatted);
+  }
 ///get all annonce by Id
   public function GetAnnonceByIdAction($id )
   {
